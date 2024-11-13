c53845feff86d605eb07f219a99873c6c2471ba2
mroreoman <830aryans@gmail.com>
2024-11-12 19:15:57 -0500

    front end work (rankings page)

--- init_db.py
+++ init_db.py
@@ -1,22 +1,26 @@
+import sys
+
 import sqlite3
 
-con = sqlite3.connect("database.db")
+if __name__ == "__main__":
+    con = sqlite3.connect("database.db")
 
-with open('schema.sql') as f:
-    con.executescript(f.read())
+    with open('schema.sql') as f:
+        con.executescript(f.read())
 
-cur = con.cursor()
+    cur = con.cursor()
 
-# player_sample = [("Hector", 1), ("Aryan", 2), ("Nabil", 300), ("Esau", 299)]
-# cur.executemany("INSERT INTO players VALUES(?, ?)", player_sample)
+    if len(sys.argv) > 1 and sys.argv[1] == "--fill":
+        player_sample = [("Hector", 1), ("Aryan", 2), ("Nabil", 300), ("Esau", 299)]
+        cur.executemany("INSERT INTO players VALUES(?, ?)", player_sample)
 
-# match_sample = [
-#     ("Hector", 0, "Aryan", 11),
-#     ("Aryan", 5, "Hector", 11),
-#     ("Nabil", 21, "Hector", 17),
-#     ("Esau", 13, "Nabil", 21)
-# ]
-# cur.executemany("INSERT INTO matches(player1,score1,player2,score2) VALUES(?, ?, ?, ?)", match_sample)
+        match_sample = [
+            ("Hector", 0, "Aryan", 11),
+            ("Aryan", 5, "Hector", 11),
+            ("Nabil", 21, "Hector", 17),
+            ("Esau", 13, "Nabil", 21)
+        ]
+        cur.executemany("INSERT INTO matches(player1,score1,player2,score2) VALUES(?, ?, ?, ?)", match_sample)
 
-con.commit()
-con.close()
+    con.commit()
+    con.close()
